b8b77526abe50a7a6beb7c1ab18633bb4856a84a
Leonel Rivero <103612900+leorivero91@users.noreply.github.com>
2022-05-02 18:12:47 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let precioInicial: number = 450.50;
+console.log("Precio inicial:" precioInicial);
+let descuento: number = (precioInicial * 0.10);
+console.log("Descuento"descuento);
+let precioFinal: number = (precioInicial - descuento);
+console.log("Precio final" precioFinal);
+
+
+
+
+
+
+
